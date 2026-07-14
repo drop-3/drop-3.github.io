6df07e2505103e6dc8ad2867fc4dd215c089364d
drop-3 <exxxtazy2010@gmail.com>
2026-07-15 00:51:43 +0300

    Update test.js

--- test.js
+++ test.js
@@ -24,7 +24,7 @@
 
     function init() {
         // Запускаем таймер на 1.5 секунды после готовности приложения
-        setTimeout(reloadStartPage, 1500);
+        setTimeout(reloadStartPage, 10000);
     }
 
     // Ждем полной загрузки интерфейса Lampa
