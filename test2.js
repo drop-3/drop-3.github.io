c841c068fab1ac39705f0f6d965d9537e8f1cec1
drop-3 <exxxtazy2010@gmail.com>
2026-07-16 02:19:14 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,4 +1,4 @@
-/* --- СТАРТ БЛОКА: Перехват JSON с защитой от дубликатов --- */
+/* --- СТАРТ БЛОКА: Финальный перехват JSON --- */
 (function () {
     'use strict';
 
@@ -7,7 +7,7 @@
         catch(e) { return []; }
     }
 
-    // 1. Кнопка "Сохранить в Лампу" с ПРОВЕРКОЙ НА ДУБЛИКАТЫ
+    // 1. Кнопка "Сохранить в Лампу" (с защитой от дубликатов и сохранением постера)
     if (window.Lampa && window.Lampa.Select) {
         var old_show = Lampa.Select.show;
         Lampa.Select.show = function(params) {
@@ -30,18 +30,15 @@
                         var list = getSaves();
                         var clean_hash = hash || magnet.replace(/.*btih:([a-zA-Z0-9]+).*/i, '$1');
                         
-                        // ЖЕСТКАЯ ПРОВЕРКА: Если такой магнет или хэш уже есть — отмена!
-                        var exists = list.some(function(i) {
-                            return i.magnet === magnet || (i.hash && i.hash === clean_hash);
-                        });
-
-                        if (exists) {
+                        // Проверка: есть ли уже такой торрент?
+                        if (list.some(function(i) { return i.magnet === magnet || (i.hash && i.hash === clean_hash); })) {
                             if (window.Lampa && Lampa.Noty) Lampa.Noty.show('⚠️ Уже есть в сохраненных!');
                             return;
                         }
 
                         var act = Lampa.Activity.active() ? (Lampa.Activity.active().activity || {}) : {};
                         
+                        // Сохраняем название, постер и хэш
                         list.unshift({
                             title: act.title || act.name || d.title || 'Без названия',
                             poster: act.poster || act.img || act.backdrop || '',
@@ -65,28 +62,65 @@
         };
     }
 
-    // 2. ПОДМЕНА ДАННЫХ (с защитой от того, что ТоррСервер загрузится позже нас)
+    // 2. Умный фильтр: распознает любой запрос к ТоррСерверу
+    function isTorrServerUrl(url) {
+        if (!url || typeof url !== 'string') return false;
+        var u = url.toLowerCase();
+        
+        // Исключаем запросы к базам фильмов (TMDB, CUB и т.д.)
+        if (u.indexOf('themoviedb') > -1 || u.indexOf('tmdb') > -1 || u.indexOf('cub.watch') > -1 || u.indexOf('jac.red') > -1) return false;
+        
+        // Проверяем порты ТоррСервера и стандартные пути API
+        if (u.indexOf('8090') > -1 || u.indexOf('8095') > -1 || u.indexOf('torrserver') > -1 || u.indexOf('mytorrents') > -1) return true;
+        if (u.indexOf('/echo') > -1 || u.indexOf('/torrents') > -1 || u.indexOf('/list') > -1 || u.indexOf('/view') > -1 || u.indexOf('/stat') > -1) return true;
+        
+        return false;
+    }
+
+    // 3. Перехватчик (работает в фоне, чтобы поймать плагины, загружаемые позже)
     setInterval(function() {
-        if (window.Lampa && window.Lampa.Torrserver) {
-            // Проверяем главные функции ТоррСервера: list, get, torrents
-            ['list', 'get', 'torrents', 'load'].forEach(function(method) {
-                var orig = Lampa.Torrserver[method];
-                if (typeof orig === 'function' && !orig._my_hook) {
-                    
-                    Lampa.Torrserver[method] = function(onSuccess, onError) {
-                        var saves = getSaves();
-                        // Если в нашей памяти есть сохранения — сразу отдаем их Лампе!
-                        if (saves.length > 0) {
-                            if (typeof onSuccess === 'function') onSuccess(saves);
-                            return;
-                        }
-                        return orig.apply(this, arguments);
-                    };
-                    
-                    Lampa.Torrserver[method]._my_hook = true;
+        if (!window.Lampa) return;
+
+        // Перехватываем все сетевые запросы Лампы (Lampa.Network)
+        if (Lampa.Network) {
+            for (var key in Lampa.Network) {
+                if (typeof Lampa.Network[key] === 'function' && !Lampa.Network[key]._my_hook) {
+                    (function(method, orig) {
+                        Lampa.Network[method] = function(url, onSuccess, onError) {
+                            if (isTorrServerUrl(url)) {
+                                var saves = getSaves();
+                                if (typeof onSuccess === 'function') onSuccess(saves);
+                                return;
+                            }
+                            return orig.apply(this, arguments);
+                        };
+                        Lampa.Network[method]._my_hook = true;
+                    })(key, Lampa.Network[key]);
                 }
-            });
+            }
         }
+
+        // Перехватываем прямые вызовы модулей ТоррСервера (Torrserver, TS, Matrod)
+        ['Torrserver', 'TS', 'Matrod', 'TorrServe'].forEach(function(mod_name) {
+            var mod = Lampa[mod_name];
+            if (mod) {
+                for (var k in mod) {
+                    if (typeof mod[k] === 'function' && k.match(/list|get|all|torrents|load|echo|view/i) && !mod[k]._my_hook) {
+                        (function(method, orig) {
+                            mod[method] = function(onSuccess, onError) {
+                                var saves = getSaves();
+                                if (saves.length > 0 && typeof onSuccess === 'function') {
+                                    onSuccess(saves);
+                                    return;
+                                }
+                                return orig.apply(this, arguments);
+                            };
+                            mod[method]._my_hook = true;
+                        })(k, mod[k]);
+                    }
+                }
+            }
+        });
     }, 500);
 
 })();
